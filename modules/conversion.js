6bd94dbc240b149fa51bc898aa43ba334457e7b0
tony <tonyofbyteball@users.noreply.github.com>
2022-06-03 18:41:46 +0300

    use exchange rates from the hub

--- modules/conversion.js
+++ modules/conversion.js
@@ -1,31 +1,20 @@
 /*jslint node: true */
 'use strict';
-const async = require('async');
-const request = require('request');
-const conf = require('ocore/conf');
 const eventBus = require('ocore/event_bus.js');
-const notifications = require('./notifications');
+const network = require('ocore/network.js');
 
 
-var GBYTE_BTC_rate;
-var BTC_USD_rate;
-var EUR_USD_rate = 1.18;
+var EUR_USD_rate = 1.07;
 var GBP_USD_rate;
 var USD_JPY_rate;
 var USD_RUR_rate;
 
 var bRatesReady = false;
+eventBus.once('rates_updated', () => {
+	bRatesReady = true;
+	checkRatesAndHeadless();
+});
 
-function checkAllRatesUpdated() {
-	if (bRatesReady)
-		return;
-	if (GBYTE_BTC_rate && BTC_USD_rate && EUR_USD_rate) {
-		bRatesReady = true;
-		console.log('rates are ready');
-		const headlessWallet = require('headless-obyte'); // start loading headless only when rates are ready
-		checkRatesAndHeadless();
-	}
-}
 
 var bHeadlessReady = false;
 eventBus.once('headless_wallet_ready', () => {
@@ -39,43 +28,12 @@ function checkRatesAndHeadless() {
 }
 
 
-function updateBittrexRates() {
-	console.log('updating bittrex');
-	const apiUri = 'https://bittrex.com/api/v1.1/public/getmarketsummaries';
-	request(apiUri, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			try {
-				var arrCoinInfos = JSON.parse(body).result;
-			}
-			catch (e) {
-				return console.log(e.toString());
-			}
-			if (!arrCoinInfos)
-				return console.log('bad rates from bittrex');
-			arrCoinInfos.forEach(coinInfo => {
-				let price = coinInfo.Last; // number
-				if (!price)
-					return;
-				if (coinInfo.MarketName === 'USDT-BTC')
-					BTC_USD_rate = price;
-				else if (coinInfo.MarketName === 'BTC-GBYTE')
-					GBYTE_BTC_rate = price;
-			});
-			checkAllRatesUpdated();
-		}
-		else {
-			notifications.notifyAdmin("getting bittrex data failed", error + ", status=" + (response ? response.statusCode : '?'));
-			console.log("Can't get currency rates from bittrex, will retry later");
-		}
-	});
-}
-
-
 
 function usdToBytes(amountInUSD) {
-	if (!bRatesReady)
+	const rates = network.exchangeRates;
+	if (!rates.GBYTE_USD)
 		throw Error("rates not ready yet");
-	return Math.round(1e9 * amountInUSD / (GBYTE_BTC_rate * BTC_USD_rate));
+	return Math.round(1e9 * amountInUSD / rates.GBYTE_USD);
 }
 
 function eurToUsd(amountInEUR){
@@ -91,12 +49,6 @@ function getUsdAmount(amount, currency){
 }
 
 
-function enableRateUpdates() {
-	setInterval(updateBittrexRates, 600 * 1000);
-}
-
-updateBittrexRates();
-enableRateUpdates();
 
 exports.usdToBytes = usdToBytes;
 exports.getUsdAmount = getUsdAmount;
